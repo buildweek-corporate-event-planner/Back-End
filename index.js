6bcf8e63fc9327a3a292ba03de4d8978ac62bbbb
Sydney Crumley <srsimps19@gmail.com>
2019-10-20 21:10:24 -0500

    commented out global vars

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const server = require('./api/server')
 const port = 8000
 
 //Set Globals
-const path = require("path");
-global.dbConfig = path.resolve(__dirname + "/data/dbConfig");
+// const path = require("path");
+// global.dbConfig = path.resolve(__dirname + "/data/dbConfig");
 
 server.listen(port, () => {
     console.log(`\n === Server running on port ${port} === \n`)
