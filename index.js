e0caaef50ec8d80e43e821b65cbd78ee89c0c1f9
Sydney Crumley <srsimps19@gmail.com>
2019-10-20 21:14:27 -0500

    added gen endpoint

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = 8000
 //Set Globals
 // const path = require("path");
 // global.dbConfig = path.resolve(__dirname + "/data/dbConfig");
-
+server.get('/', (req,res) => {
+    res.send(`<h1>Server is working</h1>`)
+})
 server.listen(port, () => {
     console.log(`\n === Server running on port ${port} === \n`)
 })
